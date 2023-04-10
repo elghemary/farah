d868b963ab10705272cc6b9259143dd2d3042bdc
Farah El Ghemary <farah.elghemary@outlook.com>
2023-04-10 07:06:34 +0000

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -47,7 +47,7 @@ export default {
   disqus: {
     on: true,
     loadingStrategy: 'button', // Options: onload, lazy, button
-    siteShortName: 'blog-danielkelly-io' // 'blog-danielkelly-io'
+    siteShortName: 'arsteminisit.disqus.com'
   },
 
   // Newsletter Subscribe
