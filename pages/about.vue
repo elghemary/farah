d7a1dfaa496bed1da3c5b20dc68d998281794d78
Farah El Ghemary <farah.elghemary@outlook.com>
2023-04-10 07:31:17 +0000

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -6,9 +6,7 @@
     ></site-hero>
     <main-section theme="sidebar-right">
       <template v-slot:default>
-        <div class="content">
-          <h3>Fake Heading</h3>
-          
+        <div class="content">          
           <p>
               Hello there, and welcome to my little corner of the internet! I'm a lover of all things math, art, and technology, and this blog is my playground for exploring the fascinating connections between these worlds.          </p>
           
