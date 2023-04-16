f82cdc692f2da11f2f08d4cccfbc901e4dda2cbc
Farah El Ghemary <farah.elghemary@outlook.com>
2023-04-16 05:37:54 +0000

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -13,11 +13,11 @@
              My name is Farah, and Welcome to my little corner of the internet! I'm a lover of all things Mathematics, Art, and Technology, and this is my playground for exploring the fascinating topics of each field.          </p>
              <br />  
              <br />
- ### How to reach me
-<a href="https://linkedin.com/in/farahelghemary" target="_blank"><img align="left" alt="elghemary | LinkedIn" width="22px" src="https://github.com/elghemary/farah/blob/main/logo/linkedin.svg" /> <br />
-<a href="https://instagram.com/elghemary1" target="_blank"><img align="left" alt="elghemary | Instagram" width="22px" src="https://github.com/elghemary/farah/blob/main/logo/Instagram.svg" /> <br />
-<a href="https://twitter.com/elghemary1" target="_blank"><img align="left" alt="elghemary | Twitter" width="22px" src="https://github.com/elghemary/farah/blob/main/logo/twitter.svg" /> <br />
-<br />
+ How to reach out to me: <br />
+ [LinkedIn](https://linkedin.com/in/farahelghemary)  <br />
+ [Instagram](https://instagram.com/elghemary1) <br />
+
+ <br />
 
         </div>
       </template>
